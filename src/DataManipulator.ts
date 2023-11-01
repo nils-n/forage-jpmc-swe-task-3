2158857f0df2331eef41ec3236e6ed858d9850d8
Nils <52067471+nils-n@users.noreply.github.com>
2023-11-01 16:19:49 +0000

    update DataManipulator and Graph to show ratios, trigger alert when ratio is out of defined interval

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,50 @@
-import { ServerRespond } from './DataStreamer';
+import { ServerRespond } from "./DataStreamer";
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  price_abc: number;
+  price_def: number;
+  ratio: number;
+  timestamp: Date;
+  upper_bound: number;
+  lower_bound: number;
+  trigger_alert: number | undefined;
 }
 
-
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    const priceABC =
+      (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2.0;
+    const priceDEF =
+      (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2.0;
+    let ratio = 1;
+    if (priceDEF > 0) {
+      ratio = priceABC / priceDEF;
+    }
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
+    // select latest timepoint
+    let newTimestamp;
+    if (serverRespond[0].timestamp > serverRespond[1].timestamp) {
+      newTimestamp = serverRespond[0].timestamp;
+    } else {
+      newTimestamp = serverRespond[1].timestamp;
+    }
+    console.log(newTimestamp);
+    // set when display a trigger alert
+    let triggerAlert;
+    if (ratio > upperBound || ratio < lowerBound) {
+      triggerAlert = ratio;
+    } else {
+      triggerAlert = undefined;
+    }
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: newTimestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: triggerAlert,
+    };
   }
 }
